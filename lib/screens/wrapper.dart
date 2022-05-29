9d8c8e0eddd2488e36d6f42e4826aa7af3cfbcd5
Arian Febrian <arianfebrian46@gmail.com>
2022-05-29 12:29:41 +0700

    #3

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'authenticate/authenticate.dart';
+import 'home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Authenticate();
+  }
+}
